25dfbe34b5bcfa6f49f0bcf36be16e1e5a2ee5e0
abhishek305 <abhishekshaji1994@gmail.com>
2020-08-13 12:09:57 +0530

    update

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+});
+
 nunjucks.configure(['views/'], {
   autoescape: false,
   express: app,
